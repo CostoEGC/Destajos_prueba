36871c7c005fe180eaaf679955b80c13b4cd4eb1
CostoEGC <costosegc@gmail.com>
2026-07-02 18:29:45 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1166,8 +1166,8 @@ elif menu == "Mapa Interactivo":
             x_min, y_min = 0.0, 0.0
             #x_max = cols * ancho_celda
             #y_max = filas * alto_celda
-            x_max = 150
-            y_max = 50
+            x_max = 200
+            y_max = 80
 
             fig_diag.add_shape(
                 type="path",
