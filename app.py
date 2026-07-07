359820d65275fcd3693c5b9e9a2bd520c45742fb
CostoEGC <costosegc@gmail.com>
2026-07-07 09:02:54 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown(
     #MainMenu {visibility: hidden;}
     header {visibility: hidden;}
     footer {visibility: hidden;}
-    .stAppToolbar {visibility: hidden;}
+    
     .stAppDeployButton {display: none;}
     </style>
     """,
