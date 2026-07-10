0565b502186ec012a53140eb479fd41cd54c5553
CostoEGC <costosegc@gmail.com>
2026-07-10 14:54:14 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1054,8 +1054,8 @@ if menu == "Registro de Destajos":
             /* Mueve el contenedor completo del botón arriba y a la derecha */
             div[data-testid="stFormSubmitButton"] {
                 position: absolute !important;
-                top: -52px !important;  /* <--- NÚMERO CLAVE PARA SUBIRLO */
-                right: 0px !important;   /* <--- LO PEGA TOTALMENTE A LA DERECHA */
+                top: -105px !important;  /* <--- NÚMERO CLAVE PARA SUBIRLO */
+                right: 20px !important;   /* <--- LO PEGA TOTALMENTE A LA DERECHA */
                 z-index: 9999 !important;
             }
             /* Le da el diseño azul y el tamaño correcto */
