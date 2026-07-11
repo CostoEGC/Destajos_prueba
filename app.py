f34bff454ea23ebd34f68539afc528df1883f252
CostoEGC <costosegc@gmail.com>
2026-07-11 09:58:48 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1943,10 +1943,9 @@ elif menu == "Mapa Interactivo":
                 with open(archivo_encontrado, "r", encoding="utf-8") as f:
                     svg_content = f.read()
 
-                try:
-                    soup = BeautifulSoup(svg_content, "xml")
-                except:
-                    soup = BeautifulSoup(svg_content, "html.parser")
+                soup = BeautifulSoup(svg_content, "html.perser")
+                
+                    
                     
                 for path_elem in soup.find_all(['path', 'polygon']):
                     path_elem['fill-rule'] = "evenodd"
