d04c72e9bcbec99516b54d2db9450fac9c4128bb
CostoEGC <costosegc@gmail.com>
2026-07-14 14:28:21 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2322,8 +2322,8 @@ elif menu == "Mapa Interactivo":
             
             x_coords, y_coords, colores_relleno, textos_hover = [], [], [], []
 
-            ancho_celda = 25
-            alto_celda = 5
+            ancho_celda = 35
+            alto_celda = 10
             
             for i, row in enumerate(df_lote_diag.itertuples()):
                 col_actual = i % cols
